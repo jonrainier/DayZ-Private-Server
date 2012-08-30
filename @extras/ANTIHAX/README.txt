d6b5805966e4d147d7d0f27fddff5051a4838f20
Jonathan Rainier <Pwnoz0r@gmail.com>
2012-08-30 19:06:35 -0400

    added BEC and DayZAntiHax
    if there is any problems launching these, please let me know on the
    forums!

---
+++ @extras/ANTIHAX/README.txt
@@ -0,0 +1,2 @@
+INSTALLATION INSTRUCTIONS/USAGE:
+http://code.google.com/p/dayz-anti-hax/wiki/InstallationInstructions
---
+++ @extras/BEC/Config/Test.bat
@@ -0,0 +1,19 @@
+@echo off
+
+:: change size of cmd window
+mode con lines=15 cols=75
+
+:: change the color of the cmd window
+color 0e
+
+:: print some messages
+echo. 
+echo *************************
+echo Hello World
+echo This is a test file
+
+:: make it stay for 10 sec
+timeout 10
+
+:: write some jokes :)
+:: start notepad.exe
