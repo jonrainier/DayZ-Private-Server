3f07b26977a272fea182c863e570da1deab4d2b0
Jonathan Rainier <Pwnoz0r@gmail.com>
2012-10-07 02:18:50 -0400

    added a remote server start (read desc)
    this just removed the MySQL db startup if you are going to connect to a
    remote db. remember before running, you need to correct the startup
    parameters to run pMain.

---
+++ @START_SERVER_REMOTE.bat
@@ -0,0 +1,31 @@
+@echo off
+echo Executing spawn script...
+.\MySQL\bin\mysql --user=SAME_AS_INI --password=SAME_AS_INI --host=SAME_AS_INI --port=SAME_AS_INI --database=SAME_AS_INI --execute="call pMain()"
+ping 127.0.0.1 -n 5 >NUL
+.\MySQL\cecho {0A}	OK.{07}
+echo.
+echo.
+echo.
+echo Starting server...
+start .\Expansion\beta\arma2oaserver.exe -mod=@dayz;@hive -name=cfgdayz -config=cfgdayz\server.cfg -cfg=cfgdayz\arma2.cfg -profiles=cfgdayz -world=Chernarus -exThreads=0 -maxMem=1024
+.\MySQL\cecho {0A}	OK.{07} 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+.\MySQL\cecho {0B}Launcher created by Pwnoz0r.{07}
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo.
+.\MySQL\cecho {0A}Leaving le launcher...{07}
+ping 127.0.0.1 -n 5 >NUL
+echo.
+echo.
+echo.
+echo.
+echo.
+exit
