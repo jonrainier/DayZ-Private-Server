77f26190b752b7f7af3f41aaf0988bc9a311fcb8
Jonathan Rainier <pwnoz0r@gmail.com>
2016-11-02 22:32:42 -0400

    1.8.8 - 11/02/2016
    * Updated all required files to support DayZMod 1.8.8

---
+++ @hive/mod.cpp
@@ -0,0 +1,5 @@
+dir = "@Hive"; 
+name = "ArmA 2: DayZ Mod Hive"; 
+picture = "z\addons\dayz_code\gui\mod.paa"; 
+actionName = "Website"; 
+action = "http://forums.dayzgame.com/"; 
--- cfgdayz/BattlEye/README.md
+++ cfgdayz/BattlEye/README.md
@@ -1,6 +1,2 @@
 Battleye-Filters
 ================
-
-Battleye Filters
-
-Latest update on publicvariable.txt at 14-Mar-2015 10:12 CET
